9b19b7218e7887f12dbd539dfc06bc2c6bee5427
Akhilendra <akhilendra1604@gmail.com>
2022-09-11 11:08:09 +0530

    fix: schedule

--- js/schedule.js
+++ js/schedule.js
@@ -4,6 +4,7 @@ content[0].classList.remove('time-content');
 
 for(let i=0;i<timeLinks.length;i++){
     timeLinks[i].addEventListener('click', (e)=>{
+        e.preventDefault();
         content.forEach(cnt=>cnt.classList.add('time-content'));
         timeLinks.forEach(btn=> btn.classList.remove('link-border'));
         timeLinks[i].classList.add('link-border');
