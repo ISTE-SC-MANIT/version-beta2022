20b4c198d615b724dd936a133c04ce058240df14
Akhilendra <akhilendra1604@gmail.com>
2022-09-21 01:16:15 +0530

    fix: responsive nav

--- js/nav.js
+++ js/nav.js
@@ -3,8 +3,16 @@ const icon=document.querySelector('.icon');
 const navContainer=document.getElementById('nav-container');
 let visible=false;
 icon.addEventListener('click', (e)=>{
-    console.log('helo');
     if(!visible){navContainer.style.transform="translateX(-5%)"; visible=true;}
     else {navContainer.style.transform = "translateX(100%)"; visible=false;}
 });
 
+const navLinks=document.querySelectorAll('.link');
+navLinks.forEach(navlink=>{
+    navlink.addEventListener('click', (e)=>{
+        if(visible){
+            visible=false;
+            navContainer.style.transform = "translateX(100%)";
+        }
+    })
+})
