d544b93180ae6e22bdbba95d509d9302a26e0a25
jake <jjk848798@gmail.com>
2022-09-24 10:57:43 +0530

    fix:faq-responsive,footer

--- js/prizes.js
+++ js/prizes.js
@@ -20,33 +20,3 @@
 // contents.forEach((c) => {
 // 	c.addEventListener('mouseout', () => {});
 // });
-
-var acc = document.getElementsByClassName('btn');
-var panel = document.getElementsByClassName('content--');
-
-for (var i = 0; i < acc.length; i++) {
-	acc[i].onclick = function () {
-		var setClasses = !this.classList.contains('active');
-		setClass(acc, 'active', 'remove');
-		setClass(panel, 'show', 'remove');
-		// this.innerHTML = 'Hide';
-
-		if (setClasses) {
-			this.classList.toggle('active');
-			this.nextElementSibling.classList.toggle('show');
-			// this.innerHTML = 'Hide';
-		} else {
-			this.innerHTML = 'View More';
-		}
-	};
-	this.innerHTML = 'View more';
-}
-
-function setClass(els, className, fnName) {
-	for (var i = 0; i < els.length; i++) {
-		els[i].classList[fnName](className);
-		// this.innerHTML = 'Hide';
-	}
-}
-
-// });
