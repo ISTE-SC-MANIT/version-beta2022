bf45cac13e444431421caa6b970ac6d759a337ed
Akhilendra <akhilendra1604@gmail.com>
2022-09-24 17:37:14 +0530

    updated: schedule

--- js/schedule.js
+++ js/schedule.js
@@ -27,3 +27,18 @@ arrowB.addEventListener('click', (e) => {
     content[i].classList.remove('time-content');
 
 });
+
+for(let i=0;i<timeLinks.length; i++){
+    timeLinks[i].addEventListener('click', e=>{
+        e.preventDefault();
+        timeLinks.forEach(link=>{
+            link.classList.remove('link-border');
+        });
+        content.forEach(cnt=>{
+            cnt.classList.add('time-content');
+        })
+        timeLinks[i].classList.add('link-border');
+        content[i].classList.remove('time-content');
+    });
+    
+}
