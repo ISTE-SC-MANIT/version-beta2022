1fbf55d58fc0e1da6bf08093a3e643d85074ef84
rk1703 <rk1703world@gmail.com>
2022-09-26 09:59:25 +0530

    fix: schedule-carousel

--- js/script.js
+++ js/script.js
@@ -88,18 +88,3 @@ particlesJS("particles-js", {
   requestAnimationFrame(update);
 
 
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 100,
-    grabCursor: true,
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-  },
-  // centeredSlides: true,
-  slidesPerView: 'auto',
-  
-  });
---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 100,
+    grabCursor: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+  },
+  // centeredSlides: true,
+  slidesPerView: 'auto',
+  
+  });
