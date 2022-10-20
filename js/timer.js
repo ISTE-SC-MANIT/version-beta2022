48d96a508859381f8a1902700cf2b442b1788faa
Akhilendra <akhilendra1604@gmail.com>
2022-10-20 19:01:03 +0530

    updated: schedule

--- js/timer.js
+++ js/timer.js
@@ -7,7 +7,7 @@ window.addEventListener('load', (event) => {
 	var today = new Date("Oct 21, 2022");
 	var newdate = new Date();
 	
-	let countDown=new Date("Oct 21, 2022");
+	let countDown=new Date("Nov 25, 2022");
 	setInterval(function () {
 		let now = new Date().getTime(),
 			distance = countDown - now;
