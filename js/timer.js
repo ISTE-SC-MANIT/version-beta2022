a80e0785dc7af1ff901aa89cf8160bfebdb47ff3
Akhilendra <akhilendra1604@gmail.com>
2022-11-12 15:32:47 +0530

    fix: timer

--- js/timer.js
+++ js/timer.js
@@ -11,7 +11,7 @@ window.addEventListener('load', (event) => {
 	setInterval(function () {
 		let now = new Date().getTime(),
 			distance = countDown - now;
-
+		if(distance<0)distance=0;
 		(document.getElementById('days').innerText = Math.floor(
 			distance / day
 		)),
