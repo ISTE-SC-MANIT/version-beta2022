28e69bdec26a26ca7ad97a7f92a679db5854136c
Jayant Dhakad <107697079+jake8989@users.noreply.github.com>
2023-10-27 10:44:19 +0530

    Update:timer

--- js/timer.js
+++ js/timer.js
@@ -7,7 +7,7 @@ window.addEventListener('load', (event) => {
 	var today = new Date('Oct 02, 2023');
 	var newdate = new Date();
 
-	let countDown = new Date('Oct 27, 2023');
+	let countDown = new Date('Oct 28, 2023');
 	// const timerCon=document.querySelector()
 	setInterval(function () {
 		let now = new Date().getTime(),
